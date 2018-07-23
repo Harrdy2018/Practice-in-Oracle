9909217f0e982eb450b208498c3bd4d69dc5003c
Harrdy <1035525823@qq.com>
2018-07-23 20:52:30 +0800

    Create create_keystone.sh

---
+++ create_keystone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# create keystone database
+mysql -u root -p <<!
+CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
+IDENTIFIED BY 'KEYSTONE_DBPASS';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
+IDENTIFIED BY 'KEYSTONE_DBPASS';
+exit
+!
