ad5eb203f815468031da4b3c98e541ba937e8603
Harrdy <1035525823@qq.com>
2018-07-31 14:41:09 +0800

    Create identity_service.sh

---
+++ ansible-openstack/roles/db/files/identity_service.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Bootstrap the Identity service
+keystone-manage bootstrap --bootstrap-password ADMIN_PASS \
+--bootstrap-admin-url http://controller:5000/v3/ \
+--bootstrap-internal-url http://controller:5000/v3/ \
+--bootstrap-public-url http://controller:5000/v3/ \
+--bootstrap-region-id RegionOne
